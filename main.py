4eab769360d7506a1d577d3e88026a018a9d7c92
ibo shoka <i.shoka09152@gmail.com>
2024-05-30 21:08:29 +0500

    class work 30.05.2024

---
+++ main.py
@@ -0,0 +1,36 @@
+#Задания
+#1
+# a = int(input('ведите число'))
+# b = 0
+# while b <= a:
+#     print(b)
+#     b += 1
+
+#2
+# a = str(input('введите любое слово: '))
+# print(a[::-1])
+
+#3
+# a = int(input('введи число которое таблица умножение ты хочешь увидеть: '))
+# b = 0
+# while b < 11:
+#     c = a * b
+#     print(a,'*',b,'=',c)
+#     b +=1 
+
+#4
+# a = int(input('chislo:'))
+# b = int(input('vtoroe chislo:'))
+# c = 0
+# for n in range(a,b,2):
+#     c +=1
+# print("нечетные: ", c)
+# c = 0
+# a += 1
+# for x in range(a,b,2):
+#     c +=1
+# print("четных :", c)
+
+#5
+# for i in range(1,10,2):
+#     print(" "*((9 - i) // 2)+ str('*')*i)
